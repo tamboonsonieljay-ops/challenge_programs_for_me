1185d5dfbb6af72075151ff1579d12a09a66c7ba
Soniel <tamboonsonieljay@gmail.com>
2026-06-15 17:29:11 +0800

    this is my file commit on batch 2 on the quotient of numbers without the decimal

---
+++ batch2_quotient_without_decimal.py
@@ -0,0 +1,12 @@
+print("\nWant to know the quotient of your numbers?")
+
+first_number = int(input("Please enter your first number: "))
+second_number = int(input("Please enter your second number: "))
+
+print("\n------------------------------------------------------------")
+
+print("Calculating the quotient of the two numbers...")
+quotient_of_numbers = first_number // second_number
+print(f"\nThe quotient of your two numbers is: {quotient_of_numbers}")
+
+print("------------------------------------------------------------")
