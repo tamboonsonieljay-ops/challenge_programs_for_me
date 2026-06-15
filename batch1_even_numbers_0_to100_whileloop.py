5ebe376b0935f24238953a0531483468b73a744a
Soniel <tamboonsonieljay@gmail.com>
2026-06-15 11:24:24 +0800

    this is my file commit on batch 1 where it will display the even numbers from 1-100

---
+++ batch1_even_numbers_0_to100_whileloop.py
@@ -0,0 +1,9 @@
+print("\nWant to identify all the even numbers from 1 to 100?\n")
+print("Say no more! Here they are:\n")
+even_number = 2
+
+while even_number <= 100:
+    print(even_number)
+    even_number += 2
+
+print("\nThat was a hard one ;)")
