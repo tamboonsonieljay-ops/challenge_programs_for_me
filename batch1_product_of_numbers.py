da73abbbdd64a62be0c9ab28e8486685fc57cf43
Soniel <tamboonsonieljay@gmail.com>
2026-06-15 10:20:08 +0800

    this is my file commit on batch 1 on the product of numbers

---
+++ batch1_product_of_numbers.py
@@ -0,0 +1,12 @@
+print("\nWant to know the product of your numbers?")
+
+first_number = int(input("Please enter your first number: "))
+second_number = int(input("Please enter your second number: "))
+
+print("\n------------------------------------------------------------")
+
+print("Calculating the product of the two numbers...")
+product_of_numbers = first_number * second_number
+print(f"\nThe product of your two numbers is: {product_of_numbers}")
+
+print("------------------------------------------------------------")
