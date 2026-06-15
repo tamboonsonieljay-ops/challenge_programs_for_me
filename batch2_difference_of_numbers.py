d733b70ef252959d13295651ecd4c14a41983c18
Soniel <tamboonsonieljay@gmail.com>
2026-06-15 17:27:01 +0800

    this is my file commit on batch 2 on the difference of numbers

---
+++ batch2_difference_of_numbers.py
@@ -0,0 +1,12 @@
+print("\nWant to know the difference of your numbers?")
+
+first_number = int(input("Please enter your first number: "))
+second_number = int(input("Please enter your second number: "))
+
+print("\n------------------------------------------------------------")
+
+print("Calculating the difference of the two numbers...")
+difference = first_number - second_number
+print(f"\nThe difference of your two numbers is: {difference}")
+
+print("------------------------------------------------------------")
