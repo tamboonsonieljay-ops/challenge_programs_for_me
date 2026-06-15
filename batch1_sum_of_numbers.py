a51d1e6e98070ba78a6a2ed299f2e08a492deec7
Soniel <tamboonsonieljay@gmail.com>
2026-06-15 10:22:47 +0800

    Batch 1 sum of numbers second commit

--- batch1_sum_of_numbers.py
+++ batch1_sum_of_numbers.py
@@ -3,10 +3,10 @@ print("\nWant to know the sum of your numbers?")
 first_number = int(input("Please enter your first number: "))
 second_number = int(input("Please enter your second number: "))
 
-print("\n------------------------------")
+print("\n------------------------------------------------------------")
 
 print("Calculating the sum of the two numbers...")
 sum_of_numbers = first_number + second_number
 print(f"\nThe sum of your two numbers is: {sum_of_numbers}")
 
-print("------------------------------")
+print("------------------------------------------------------------")
