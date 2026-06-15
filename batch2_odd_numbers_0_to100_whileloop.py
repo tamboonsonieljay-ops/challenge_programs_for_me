3dc48e47bc0b39e013fb9bd74dcb776b22107ee8
Soniel <tamboonsonieljay@gmail.com>
2026-06-15 17:42:49 +0800

    this is my file commit on batch 2 where it will display all the odd numbers from 1-100

---
+++ batch2_odd_numbers_0_to100_whileloop.py
@@ -0,0 +1,9 @@
+print("\nWant to identify all the odd numbers from 1 to 100?\n")
+print("Say no more! Here they are:\n")
+odd_number = 1
+
+while odd_number <= 100:
+    print(odd_number)
+    odd_number += 2
+
+print("\n....that was a hard one ;)")
