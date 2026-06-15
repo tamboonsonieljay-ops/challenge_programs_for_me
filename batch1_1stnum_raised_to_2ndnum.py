c2340c62fafb95f5811f13c6a1549e9e2b48a1a4
Soniel <tamboonsonieljay@gmail.com>
2026-06-15 10:34:57 +0800

    this is my file commit on batch 1 where the first number is raised to the second number

---
+++ batch1_1stnum_raised_to_2ndnum.py
@@ -0,0 +1,12 @@
+print("\nWant to know the result of raising your first number to the power of your second number?")
+
+first_number = int(input("Please enter your first number: "))
+second_number = int(input("Please enter your second number: "))
+
+print("\n------------------------------------------------------------")
+
+print("Calculating the result...")
+result = first_number ** second_number
+print(f"\nThe result of raising {first_number} to the power of {second_number} is: {result}")
+
+print("------------------------------------------------------------")
