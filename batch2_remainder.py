aff63bc84e765081b427c7a55bea9177af916bdc
Soniel <tamboonsonieljay@gmail.com>
2026-06-15 17:30:09 +0800

    this is my file commit on batch 2 on the remiander of two numbers

---
+++ batch2_remainder.py
@@ -0,0 +1,12 @@
+print("\nWant to know the remainder of your numbers?")
+
+first_number = int(input("Please enter your first number: "))
+second_number = int(input("Please enter your second number: "))
+
+print("\n------------------------------------------------------------")
+
+print("Calculating the remainder of the two numbers...")
+remainder = first_number % second_number
+print(f"\nThe remainder of your two numbers is: {remainder}")
+
+print("------------------------------------------------------------")
